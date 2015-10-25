d007311dfd78a95495465e059ef7acb40ebac882
TimAsus <TimJAllen.ta@gmail.com>
2015-10-24 22:45:46 -0700

    7

--- src/Hash.java
+++ src/Hash.java
@@ -6,31 +6,31 @@ import java.util.TreeSet;
 
 public class Hash {
 	public static void main(String[] args) {
-		Map<Integer, TreeSet<String>> map = new TreeMap<Integer, TreeSet <String>>();
+		Map<Integer, TreeSet<String>> map = new TreeMap<Integer, TreeSet<String>>();
 		Scanner in = new Scanner(System.in);
 		while (in.hasNext()) {
 			TreeSet<String> set = new TreeSet<String>();
 			String s = in.next();
 			int h = s.hashCode();
-			
+
 			if (!map.containsKey(h)) {
 				map.put(h, set);
 				set.add(s);
 			} else {
 				set = new TreeSet<String>();
 				set.add(s);
-				map.put(h, set);	
-			
-			
-			Set<Integer> keyset = map.keySet();
-			
-			for (int k : keyset) {
-				TreeSet<String> resultSet = map.get(k);
-				if (resultSet.size() > 1)  
-					System.out.println(keyset + ":");
-				for (String words : resultSet)
-					System.out.println(words + ",");
-			}}
+				map.put(h, set);
+
+				Set<Integer> keyset = map.keySet();
+
+				for (int k : keyset) {
+					TreeSet<String> resultSet = map.get(k);
+					if (resultSet.size() > 1)
+						System.out.println(k + ":");
+					for (String words : resultSet)
+						System.out.println(words + ",");
+				}
+			}
 		}
 	}
 }
